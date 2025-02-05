4486153468cd2c9fc18ef481252213ab1f9d1a24
vadaypaul <p3pol@hotmail.com>
2025-02-05 11:41:53 -0600

    Update __init__.py

--- chatbot_gns/__init__.py
+++ chatbot_gns/__init__.py
@@ -3,7 +3,6 @@ import logging
 # Importación de módulos clave del chatbot
 from .app_gns import process_gns_entry, handle_gns, enviar_mensaje, conectar_db
 from .gpt_gns import interpretar_mensaje
-from .logic_gns import consultar_disponibilidad  # Si aún se usa esta función
 from .messaging_gns import procesar_mensaje
 from .reporting_gns import generar_reporte_actividad  # Suponiendo que genera logs o métricas
 
@@ -17,7 +16,6 @@ __all__ = [
     "enviar_mensaje",
     "interpretar_mensaje",
     "conectar_db",
-    "consultar_disponibilidad",
     "procesar_mensaje",
     "generar_reporte_actividad"
 ]
