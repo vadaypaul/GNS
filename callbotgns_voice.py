9897c3af9aa009788a51b73c0be266ae0b9e9fc8
vadaypaul <p3pol@hotmail.com>
2025-02-21 01:01:01 -0600

    Update callbotgns_voice.py

--- callbotgns_voice.py
+++ callbotgns_voice.py
@@ -13,7 +13,7 @@ def voice():
     response = VoiceResponse()
     response.say(
         "Hola, bienvenido a BarberShop GNS, ¿gustas agendar una cita o requieres otro tipo de información?",
-        voice="Polly.Lupe",  # Cambia a una voz hiperrealista de Amazon Polly
+        voice="Polly.Mia",  # Cambia a una voz hiperrealista de Amazon Polly
         language="es-MX"
     )
     response.gather(input="speech", action="/transcription", timeout=8, speechTimeout="auto", language="es-MX")
