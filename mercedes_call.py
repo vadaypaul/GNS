5c7a6ae3464b06f28c90d1fc7e4d39251bf0e1e9
vadaypaul <p3pol@hotmail.com>
2025-02-23 14:18:19 -0600

    Update mercedes_call.py

--- mercedes_call.py
+++ mercedes_call.py
@@ -1,9 +1,8 @@
 import os
 import requests
 import subprocess
-from flask import Flask, request, Response
+from flask import Flask, request, Response, send_file
 from dotenv import load_dotenv
-from flask import send_file
 
 load_dotenv()
 
@@ -34,6 +33,10 @@ def serve_audio():
     audio = generar_audio(texto)
     return Response(audio, mimetype="audio/mpeg") if audio else ("Error generando audio", 500)
 
+@app.route('/mensaje-fijo.mp3')
+def serve_mensaje_fijo():
+    return send_file("mensaje-fijo.mp3", mimetype="audio/mpeg")
+
 @app.route('/run', methods=['POST'])
 def run_voice_script():
     try:
@@ -44,7 +47,3 @@ def run_voice_script():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-@app.route('/mensaje-fijo.mp3')
-def serve_mensaje_fijo():
-    return send_file("mensaje-fijo.mp3", mimetype="audio/mpeg")
